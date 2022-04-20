5d5a7a11fdd221d8b2c6a83dcdb21d706650465f
Mees van Dijk <mees@mees.io>
2022-04-20 11:59:31 +0200

    Change deploy script to work locally
    The (evil) people at Travis CI have decided to break my CI and start
    charging me for it. So I've decided to deploy from my own laptop.

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,9 @@ npm install
 export POST_DIR=$(pwd)/../entries
 export SITE_URL="https://journal.mees.io"
 npm run build
+cd ..
+git checkout gh-pages
+cp -R journal/public/ .
+rm -rf journal
+git push origin gh-pages
+git checkout master
