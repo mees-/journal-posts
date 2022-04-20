051c76340bfeb82427fb70b058d84fe324223c00
Mees van Dijk <mees@mees.io>
2022-04-20 12:40:38 +0200

    Finally get deploy script working

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-git clone https://github.com/mees-/journal journal
-cd journal
+git clone https://github.com/mees-/journal .journal
+cd .journal
 npm install
 export POST_DIR=$(pwd)/../entries
 export SITE_URL="https://journal.mees.io"
 npm run build
 cd ..
-git checkout gh-pages
-cp -R journal/public/ .
-rm -rf journal
+git checkout -f gh-pages
+mv CNAME .CNAME
+rm -rf ./*
+cp -R .journal/public/ .
+mv .CNAME CNAME
+rm -rf .journal
+git add --all
+git commit -m "$(git log master -1 --pretty=%B)"
 git push origin gh-pages
 git checkout master
